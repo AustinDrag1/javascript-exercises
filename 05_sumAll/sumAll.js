88b00621a25e143b167fd1b552b46a5eafb0d678
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-01 23:33:41 -0500

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function( num1, num2 ) {
 
+    // Error conditions
+    let notInts = !Number.isInteger(num1) || !Number.isInteger(num2);
+    let negative = num1 < 0 || num2 < 0;
+
+    if( notInts || negative )
+    {
+        return "ERROR";
+    }
+
+    // Loop from little to small
+    let lilNum = Math.min(num1, num2);
+    let bigNum = Math.max(num1, num2);
+
+    result = 0;
+    for( let i = lilNum; i < bigNum + 1; i++ )
+    {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
