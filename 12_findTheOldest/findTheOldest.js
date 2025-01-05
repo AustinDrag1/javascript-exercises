f30d1e668ccc9ec0241e5b9f8319e703ca4d7aeb
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-05 00:20:21 -0500

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function( peeps ) {
+    return find1(peeps);
 };
+
+// Find using reduce
+const find1 = function( peeps ) {
+    return peeps.reduce( (obj, peep) => get1(obj,peep) );
+}
+
+// Calc - calculates all but the first person's age twice unfortunately
+const get1 = function( obj, peep ) {
+    
+    // Calc age (current)
+    let yearOfDeath = obj.yearOfDeath === undefined ? new Date().getFullYear() : obj.yearOfDeath;
+    const ageCurr = yearOfDeath - obj.yearOfBirth;
+
+    // Calc age (next)
+    yearOfDeath = peep.yearOfDeath === undefined ? new Date().getFullYear() : peep.yearOfDeath;
+    const ageNext = yearOfDeath - peep.yearOfBirth;
+
+    // Compare ages; return oldest
+    return ageNext > ageCurr ? peep : obj;
+}
+
+// Find using other method
+const find2 = function( peeps ) {
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
