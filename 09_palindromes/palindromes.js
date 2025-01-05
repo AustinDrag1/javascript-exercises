6b105328507ecbfb0bcc8d72d35e66212ba0fd4d
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 22:51:02 -0500

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+const palindromes = function ( letters ) {
 
+    // Remove whitespace and some punctuation
+    letters = letters.replace(/(\.|,|\ |\!)/g,"").toLowerCase();
+
+    // console.log(letters);
+
+    // Do the compares
+    const len = letters.length;
+    const stop = letters.length % 2;
+    const minChecks = len / 2 - stop;
+
+    // console.log(len);
+    // console.log(stop);
+    // console.log(minChecks);
+
+    for( let i = 0; i < minChecks; i++ )
+    {
+        // console.log("compare");
+        // console.log(letters.charAt(i));
+        // console.log(letters.charAt(len - i - 1));
+
+        if( letters.charAt(i) !== letters.charAt(len - i - 1) )
+        {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
