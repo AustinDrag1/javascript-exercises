1ecf07eceb5299c296387f17b24a5d049fa07dd5
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 23:42:44 -0500

    Complete 11

--- 11_getTheTitles/getTheTitles.spec.js
+++ 11_getTheTitles/getTheTitles.spec.js
@@ -16,3 +16,25 @@ describe('getTheTitles', () => {
     expect(getTheTitles(books)).toEqual(['Book','Book2']);
   });
 });
+
+
+describe('getTheTitles 2', () => {
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      },
+      {
+        title: 'Book3',
+        author: 'Name3'
+      }
+    ]
+
+  test('gets titles 2', () => {
+    expect(getTheTitles(books)).toEqual(['Book','Book2','Book3']);
+  });
+});
