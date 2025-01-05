4828b47482acd7ac1193b1c030347f7734783608
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 20:36:19 -0500

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce( (total, num) => total += num, 0 );
 };
 
-const multiply = function() {
+const multiply = function(nums) {
 
+  let total = 1;
+
+  for( let i = 0; i < nums.length; i++ )
+  {
+    total *= nums[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+
+  let total = 1;
+
+  for( let i = 1; i <= x; i++ )
+  {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
