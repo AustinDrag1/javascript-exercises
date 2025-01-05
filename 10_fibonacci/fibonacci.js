1cad4415c4e00714e630d12d13270a6f8e962ac5
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-04 23:36:04 -0500

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function( entry ) {
 
+    const val = Number(entry);
+
+    if( val === NaN || val !== parseInt(entry) || val < 0 )
+    {
+        return "OOPS";
+    }
+
+    const seq = [0,1];
+
+    do
+    {
+        // Exit case
+        if( seq.length > val ) { return seq[val]; }
+        
+        // Calculate next index
+        let i = seq.length - 1;
+        seq.push( seq[i] + seq[i-1] );
+    } while( true );
 };
 
 // Do not edit below this line
